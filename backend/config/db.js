2d3f7cb8cf386e8fb1820d7fe7bdd85b02dac1f1
guillaumelhullier <89132296+guillaumelhullier@users.noreply.github.com>
2022-10-11 16:35:08 +0200

    Connection MongoDb

---
+++ backend/config/db.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+mongoose
+  .connect(
+    "mongodb+srv://guillaume:particule@cluster0.zzzxaan.mongodb.net/groupomania",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    }
+  )
+
+  .then(() => console.log("Connected to MongoDb"))
+  .catch((err) => console.log("Failed to connect to MongoDb", err));
--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 require("dotenv").config({ path: "./config/.env" });
+require('./config/db');
 const app = express();
 
 const indexRoute = require("./routes/index");
