49bfaccbd57bc24dcae503e4814425866b398d7f
guillaumelhullier <89132296+guillaumelhullier@users.noreply.github.com>
2022-10-28 17:47:21 +0200

    Image Profil

--- backend/controllers/upload.controller.js
+++ backend/controllers/upload.controller.js
@@ -1,10 +1,10 @@
 const UserModel = require("../models/user.model");
 const { uploadErrors } = require("../utils/errors.utils");
 const sharp = require("sharp");
+const { Error } = require("mongoose");
 
 module.exports.uploadProfil = async (req, res) => {
   const fileName = req.body.name + ".jpg";
-
   try {
     if (
       req.file.mimetype !== "image/jpg" &&
@@ -18,12 +18,21 @@ module.exports.uploadProfil = async (req, res) => {
     const errors = uploadErrors(err);
     return res.status(201).json({ errors });
   }
+  await sharp(req.file.buffer)
+    .resize({ width: 150, height: 150 })
+    .toFile(`${__dirname}/../client/public/uploads/profil/${fileName}`);
+
   try {
-    await sharp(req.file.buffer)
-      .resize({ width: 150, height: 150 })
-      .toFile(`{__dirname}/../client/public/uploads/profil/${fileName}`);
-    res.status(201).send("Photo de profil chargée avec succès");
+    await UserModel.findByIdAndUpdate(
+      req.body.userId,
+      { $set: { picture: "./uploads/profil/" + fileName } },
+      { new: true, upsert: true, setDefaultsOnInsert: true },
+      (err, docs) => {
+        if (!err) return res.send(docs);
+        else return res.status(500).send({ message: err });
+      }
+    );
   } catch (err) {
-    res.status(400).send(err);
+    return res.status(500).send({ message: Error });
   }
 };
